bfa2f98adbb334c1aeeb4e988f752b68410bcfc8
notminhaj <ehsanuddinsyed@gmail.com>
2025-11-13 11:34:09 +1100

    Random nn update

--- nn_mode/nn.py
+++ nn_mode/nn.py
@@ -1,23 +1,3 @@
-from transformers import pipeline
-classifier = pipeline("zero-shot-classification",
-                      model="facebook/bart-large-mnli")
-
-sequence_to_classify = "one day I will see the world"
-candidate_labels = ['travel', 'cooking', 'dancing']
-classifier(sequence_to_classify, candidate_labels)
-#{'labels': ['travel', 'dancing', 'cooking'],
-# 'scores': [0.9938651323318481, 0.0032737774308770895, 0.002861034357920289],
-# 'sequence': 'one day I will see the world'}
-
-candidate_labels = ['travel', 'cooking', 'dancing', 'exploration']
-classifier(sequence_to_classify, candidate_labels, multi_label=True)
-#{'labels': ['travel', 'exploration', 'dancing', 'cooking'],
-# 'scores': [0.9945111274719238,
-#  0.9383890628814697,
-#  0.0057061901316046715,
-#  0.0018193122232332826],
-# 'sequence': 'one day I will see the world'}
-
 # pose sequence as a NLI premise and label as a hypothesis
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
 nli_model = AutoModelForSequenceClassification.from_pretrained('facebook/bart-large-mnli')
@@ -36,5 +16,3 @@ logits = nli_model(x.to(device))[0]
 entail_contradiction_logits = logits[:,[0,2]]
 probs = entail_contradiction_logits.softmax(dim=1)
 prob_label_is_true = probs[:,1]
-
-
